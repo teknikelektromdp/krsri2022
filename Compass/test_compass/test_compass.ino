f3574dd67eb4a6c441aaa36cece8c4b0ed7d057e
teknikelektromdp <103289258+teknikelektromdp@users.noreply.github.com>
2022-06-03 18:29:52 +0700

    Update test_compass.ino

--- Compass/test_compass/test_compass.ino
+++ Compass/test_compass/test_compass.ino
@@ -2,7 +2,7 @@
 
 QMC5883LCompass compass;
 
-int min_range = 253, max_range = 260;
+int min_range_south, max_range_south, min_range_north, max_range_north, min_range_east, max_range_east, min_range_west, max_range_west;
 //Get azimuth value
 int readCompass()
 {
@@ -13,18 +13,33 @@ void compassCalibration()
 {
   compass.setCalibration(-891, 1147, -1741, 433, -177, 250);  
 }
+void initAzimuth()
+{
+  min_range_south = 253;
+  max_range_south = 260;
+  min_range_north = 0;
+  max_range_north = 0;
+  min_range_east = 0;
+  max_range_east = 0;
+  min_range_west = 0;
+  min_range_west = 0;
+}
+void adjustRobotPosition()
+{
+  while(!(readCompass() >= min_range_south && readCompass() <= max_range_south))
+  {
+    compass.read();
+    Serial.println(readCompass());  
+  }
+  delay(1000);
+}
 void setup() {
   Serial.begin(9600);
   compass.init();
   compassCalibration(); 
   compass.read();
-  Serial.println(readCompass()); 
-  while(!(readCompass() >= min_range && readCompass() <= max_range))
-  {
-    compass.read();
-    Serial.println(readCompass());  
-  }
-  delay(5000);
+  initAzimuth();
+  adjustRobotPosition();
 }
 void loop() {
   Serial.println("Main Program");
